58a45ee09cf799c01a43ce02557483cb13ceac8a
robin <robin.prillwitz@gmail.com>
2022-06-08 12:42:42 +0200

    added platform sprite

--- src/main/java/com/javafwp/game/gameObjects/plattform.java
+++ src/main/java/com/javafwp/game/gameObjects/plattform.java
@@ -1,10 +1,10 @@
 package com.javafwp.game.gameObjects;
 
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class plattform extends object{
-    public plattform(double xPos, double yPos, double length, double height, Color color) {
-        super(xPos, yPos, length, height, color);
+    public plattform(double xPos, double yPos, double length, double height, Paint paint) {
+        super(xPos, yPos, length, height, paint);
         this.type = com.javafwp.game.ownTypes.type.plattform;
     }
 
